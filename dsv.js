0be9a8caa85d786b05d95540aeec3419ddd980d6
Mathias Zechmeister <zechmeister@astro.physik.uni-goettingen.de>
2021-08-10 12:30:58 +0200

    new: Basic files.

---
+++ dsv.js
@@ -0,0 +1,47 @@
+async function dsv(file, ...args){
+    if (typeof file == "string") {
+        // nothing needed if file is instanceof File
+        var file = await fetch(file);
+    }
+    var responseText = await file.text()
+    if (responseText.length) {
+        responseText.__proto__.basename = file.name || file.url.split('/').pop()
+        text_to_table(responseText, ...args)
+    }
+}
+
+function auto_colnames(data){
+    // check for a header row (no numbers) and convert it to column names
+    colnames = data.map(d => d[0])
+    data._colnames = colnames   // null
+    all_text = colnames.every(x => isNaN(Number(x)))   // check if there is any number
+    if (all_text) {
+        data._colnames = colnames
+        for (var i=0; i<colnames.length; i += 1) {
+            data[i].shift()   // remove headline
+            data[colnames[i]] = data[i]   // data can be now index by column number or column name
+        }
+    }
+    else {
+        data._colnames = [...Array(data.length+1).keys()] 
+    }
+    data.unshift([...Array(data[0].length).keys()])  // prepend a column with row numbers
+    console.log(colnames)
+}
+
+function text_to_table(text, func, delim){
+   // guess the delimiter if not given
+   lines = text.split("\n");
+   if (delim) {}
+   else if (lines[0].split(";").length > 1) {delim = ";"}
+   else if (lines[0].split(",").length > 1) {delim = ","}
+   else if (lines[0].split(RegExp(" +")).length > 1) {delim = RegExp(" +")}
+   console.log("delim:", delim)
+   data = lines.map(x => x.split(delim));
+   // transpose
+   data = data[0].map((_,k) => data.map(hjk => ""+hjk[k]))
+   auto_colnames(data)
+   data.basename = text.basename
+   func(data)
+}
+
---
+++ zoom_pan.js
@@ -0,0 +1,76 @@
+function zoompan(){
+var graph = document.getElementsByClassName("js-plotly-plot")[0];
+
+var update;
+
+function pan(axis, dx, mode=1) {
+    axis += 'axis';
+    var [min, max] = graph._fullLayout[axis].range;
+    if (typeof min == "string") {
+        min = new Date(min).getTime();
+        max = new Date(max).getTime();
+    }
+    dx *= max - min;
+    update[axis+'.range'] = [min+dx, max+mode*dx];
+}
+
+function panX(dx) {pan('x', dx)}
+function panY(dy) {pan('y', dy)}
+function zoomX(dx) {pan('x', dx, -1)}
+function zoomY(dy) {pan('y', dy, -1)}
+
+function current_extreme(y, x, xbeg, xend) {
+    yf = y.filter((yi, i) => xbeg<x[i] & x[i]<xend)
+    return [Math.min(...yf), Math.max(...yf)]
+}
+
+graph.tabIndex = 0; // https://stackoverflow.com/questions/3149362/capture-key-press-or-keydown-event-on-div-element
+graph.onmouseover = graph.focus   // focus when over, thus no click needed
+graph.style.cssText = "resize:both; overflow: auto;" // border: 1px solid; height:250px"
+
+
+graph.addEventListener("keydown", function(e){
+    if (e.target!=this) {return} // e.g. to escape to edittext
+    var key = e.key;
+    if (e.ctrlKey) key = 'Ctrl+' + key;
+    console.log(e, key);
+    var fac = 0.1;   // pan and zoom factor
+    update = {};
+    var extremes = graph._fullData[0]._extremes;  // only first data set
+    switch (key) {
+        case 'Ctrl+ArrowRight': zoomX(fac); break;
+        case 'Ctrl+ArrowLeft': zoomX(-fac); break;
+        case 'Ctrl+ArrowUp': zoomY(fac); break;
+        case 'Ctrl+ArrowDown': zoomY(-fac); break;
+        case '+': zoomX(fac); zoomY(fac); break;
+        case '-': zoomX(-fac); zoomY(-fac); break;
+        case 'X': case 'U':
+             update['xaxis.range'] = [extremes.x.min[0].val, extremes.x.max[0].val];
+        case 'Y': case 'U':
+             update['yaxis.range'] = [extremes.y.min[0].val, extremes.y.max[0].val]; break;
+        case 'x':
+             update['xaxis.range'] = [extremes.x.min[0].val, extremes.x.max[0].val];
+        case 'y':
+             yrange = graph._fullData.map(d => current_extreme(d.y,d.x,...graph._fullLayout.xaxis.range)).reduce((e1,e2) => [Math.min(e1[0],e2[0]), Math.max(e1[1],
+ e2[1])]);
+             update['yaxis.range'] = yrange; break;
+        case 'u':
+             update['xaxis.autorange'] = true;
+             update['yaxis.autorange'] = true; break;
+        case '0': update['yaxis.range[0]'] = 0; break;
+        case 'ArrowRight': panX(fac); break;
+        case 'ArrowLeft': panX(-fac); break;
+        case 'ArrowUp': panY(fac); break;
+        case 'ArrowDown': panY(-fac); break;
+        case 'Home': panX(-1.); break;
+        case 'End': panX(1.); break;
+        case 'PageUp': panY(1.); break;
+        case 'PageDown': panY(-1.); break;
+        default: return;
+    }
+
+    Plotly.relayout(graph, update);
+    return false
+});
+}
+
