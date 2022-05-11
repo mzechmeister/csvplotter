3aa874fcb6ac9a19672d05ee5755c97b13cfafb2
Mathias Zechmeister <zechmeister@astro.physik.uni-goettingen.de>
2022-05-11 23:32:41 +0200

    style: Trailing semilons removed.

--- zoom_pan.js
+++ zoom_pan.js
@@ -1,17 +1,17 @@
 function zoompan(){
-var graph = document.getElementsByClassName("js-plotly-plot")[0];
+var graph = document.getElementsByClassName("js-plotly-plot")[0]
 
-var update;
+var update
 
 function pan(axis, dx, mode=1) {
-    axis += 'axis';
-    var [min, max] = graph._fullLayout[axis].range;
+    axis += 'axis'
+    var [min, max] = graph._fullLayout[axis].range
     if (typeof min == "string") {
-        min = new Date(min).getTime();
-        max = new Date(max).getTime();
+        min = new Date(min).getTime()
+        max = new Date(max).getTime()
     }
-    dx *= max - min;
-    update[axis+'.range'] = [min+dx, max+mode*dx];
+    dx *= max - min
+    update[axis+'.range'] = [min+dx, max+mode*dx]
 }
 
 function panX(dx) {pan('x', dx)}
@@ -36,7 +36,7 @@ function trac_mouse(evt) {
     j0 = evt.y
 }
 
-graph.tabIndex = 0; // https://stackoverflow.com/questions/3149362/capture-key-press-or-keydown-event-on-div-element
+graph.tabIndex = 0   // https://stackoverflow.com/questions/3149362/capture-key-press-or-keydown-event-on-div-element
 graph.onmouseover = graph.focus   // focus when over, thus no click needed
 graph.style.cssText = "resize:both; overflow: auto;" // border: 1px solid; height:250px"
 
@@ -44,51 +44,51 @@ var mousei
 
 graph.addEventListener("keydown", function(e){
     if (e.target!=this) {return} // e.g. to escape to edittext
-    var key = e.key;
-    if (e.ctrlKey) key = 'Ctrl+' + key;
-    console.log(e, key);
-    var fac = 0.1;   // pan and zoom factor
-    update = {};
-    var extremes = graph._fullData[0]._extremes;  // only first data set
+    var key = e.key
+    if (e.ctrlKey) key = 'Ctrl+' + key
+    console.log(e, key)
+    var fac = 0.1   // pan and zoom factor
+    update = {}
+    var extremes = graph._fullData[0]._extremes  // only first data set
     switch (key) {
-        case 'Ctrl+ArrowRight': zoomX(fac); break;
-        case 'Ctrl+ArrowLeft': zoomX(-fac); break;
-        case 'Ctrl+ArrowUp': zoomY(fac); break;
-        case 'Ctrl+ArrowDown': zoomY(-fac); break;
-        case '+': zoomX(fac); zoomY(fac); break;
-        case '-': zoomX(-fac); zoomY(-fac); break;
+        case 'Ctrl+ArrowRight': zoomX(fac); break
+        case 'Ctrl+ArrowLeft': zoomX(-fac); break
+        case 'Ctrl+ArrowUp': zoomY(fac); break
+        case 'Ctrl+ArrowDown': zoomY(-fac); break
+        case '+': zoomX(fac); zoomY(fac); break
+        case '-': zoomX(-fac); zoomY(-fac); break
         case 'X': case 'U':
-             update['xaxis.range'] = [extremes.x.min[0].val, extremes.x.max[0].val];
+             update['xaxis.range'] = [extremes.x.min[0].val, extremes.x.max[0].val]
              if (key=='X') break
         case 'Y': case 'U':
-             update['yaxis.range'] = [extremes.y.min[0].val, extremes.y.max[0].val]; break;
+             update['yaxis.range'] = [extremes.y.min[0].val, extremes.y.max[0].val]; break
         case 'x':
              let xrange = graph._fullData.map(d => current_extreme(d.x,d.y,...graph._fullLayout.yaxis.range)).reduce((e1,e2) => [Math.min(e1[0],e2[0]), Math.max(e1[1],
  e2[1])])
-             update['xaxis.range'] = xrange; break;
+             update['xaxis.range'] = xrange; break
         case 'y':
              let yrange = graph._fullData.map(d => current_extreme(d.y,d.x,...graph._fullLayout.xaxis.range)).reduce((e1,e2) => [Math.min(e1[0],e2[0]), Math.max(e1[1],
- e2[1])]);
-             update['yaxis.range'] = yrange; break;
+ e2[1])])
+             update['yaxis.range'] = yrange; break
         case 'u':
-             update['xaxis.autorange'] = true;
-             update['yaxis.autorange'] = true; break;
-        case '0': update['yaxis.range[0]'] = 0; break;
-        case 'ArrowRight': panX(fac); break;
-        case 'ArrowLeft': panX(-fac); break;
-        case 'ArrowUp': panY(fac); break;
-        case 'ArrowDown': panY(-fac); break;
-        case 'Home': panX(-1.); break;
-        case 'End': panX(1.); break;
-        case 'PageUp': panY(1.); break;
-        case 'PageDown': panY(-1.); break;
+             update['xaxis.autorange'] = true
+             update['yaxis.autorange'] = true; break
+        case '0': update['yaxis.range[0]'] = 0; break
+        case 'ArrowRight': panX(fac); break
+        case 'ArrowLeft': panX(-fac); break
+        case 'ArrowUp': panY(fac); break
+        case 'ArrowDown': panY(-fac); break
+        case 'Home': panX(-1.); break
+        case 'End': panX(1.); break
+        case 'PageUp': panY(1.); break
+        case 'PageDown': panY(-1.); break
         case 'l': case 'L':
               // toggle linear and log scale
               axis = graph.layout[key=='L' ? 'xaxis' : 'yaxis'];   // keep format
-              [func, axis.type] = (axis.type == 'linear') ? [Math.log10, 'log'] : [(x => 10**x), 'linear'];
+              [func, axis.type] = (axis.type == 'linear') ? [Math.log10, 'log'] : [(x => 10**x), 'linear']
               axis['range'] = axis['range'].map(func)   // adjust the range
               update = {axis}
-              break;
+              break
         case 'g':
               // toggle grid
               var showgrid = graph.layout.yaxis.showgrid == false
@@ -107,10 +107,10 @@ graph.addEventListener("keydown", function(e){
                   break
               }
 
-              var xaxis = graph._fullLayout.xaxis;
-              var yaxis = graph._fullLayout.yaxis;
-              var l = graph._fullLayout.margin.l;
-              var t = graph._fullLayout.margin.t;
+              var xaxis = graph._fullLayout.xaxis
+              var yaxis = graph._fullLayout.yaxis
+              var l = graph._fullLayout.margin.l
+              var t = graph._fullLayout.margin.t
               x0 = xaxis.p2c(i0 - l + window.scrollX)
               y0 = yaxis.p2c(j0 - t + window.scrollY)
               update.annotations = [{
@@ -130,8 +130,8 @@ graph.addEventListener("keydown", function(e){
               graph.onmousemove = function(evt) {
                   mousei = evt.x + window.scrollX
                   mousej = evt.y + window.scrollY
-                  mouseX = xaxis.p2c(mousei - l);
-                  mouseY = yaxis.p2c(mousej - t);
+                  mouseX = xaxis.p2c(mousei - l)
+                  mouseY = yaxis.p2c(mousej - t)
                   mouseX = clamp(mouseX, ...xaxis.range)
                   mouseY = clamp(mouseY, ...yaxis.range)
                   rulertext = `[${x0.toPrecision(7)}, ${y0.toPrecision(7)}] ${mouseX.toPrecision(7)}, ${mouseY.toPrecision(7)}  distance: ${(mouseX-x0).toPrecision(7)}, ${(mouseY-y0).toPrecision(7)}`
@@ -140,14 +140,14 @@ graph.addEventListener("keydown", function(e){
                   {annotations:
                    [{x: x0, y: y0, ax: mouseX, ay: mouseY, axref:'x', ayref:'y', arrowhead: 7, arrowwidth: 1, text: ''},
                     {x: 1, y: 0, ax: 0, ay: 0, xref:'paper', yref:'paper', showarrow: false, xanchor: "right", yanchor: "bottom", text: rulertext}]})
-              };
+              }
               break
-        default: return;
+        default: return
     }
 
-    Plotly.relayout(graph, update);
+    Plotly.relayout(graph, update)
     return false
-});
+})
 
 
 
@@ -166,7 +166,7 @@ function rubber_init(e) {
     rx0 = xaxis.range
     ry0 = yaxis.range
     obj.onmousemove = rubber_zoom
-    obj.onmouseup = function(e) {
+    obj.onmouseup = function() {
         obj.onmousemove = null
     }
 }
@@ -190,6 +190,6 @@ graph.addEventListener('mousedown', function(evt){
     // better would be https://github.com/plotly/plotly.js/issues/4004
     if (evt.buttons == 4) this._fullLayout.dragmode = 'pan'
     if (evt.buttons == 2) rubber_init(evt)
-}, true);
+}, true)
 
 }
