6023e4b56d4b67901882aa4b478e1750ddf9615c
Mathias Zechmeister <zechmeister@astro.physik.uni-goettingen.de>
2022-12-25 09:51:08 +0100

    chore: Function auto_colnames revised.

--- dsv.js
+++ dsv.js
@@ -1,4 +1,4 @@
-async function dsv(file, ...args){
+async function dsv(file, ...args) {
     if (typeof file == "string") {
         // nothing needed if file is instanceof File
         var file = await fetch(file);
@@ -11,26 +11,24 @@ async function dsv(file, ...args){
     }
 }
 
-function auto_colnames(data){
+function auto_colnames(data) {
     // check for a header row (no numbers) and convert it to column names
     colnames = data.map(d => d[0])
-    data._colnames = colnames   // null
-    all_text = colnames.every(x => isNaN(Number(x)))   // check if there is any number
-    if (all_text) {
-        data._colnames = colnames
-        for (var i=0; i<colnames.length; i += 1) {
-            data[i].shift()   // remove headline
-            data[colnames[i]] = data[i]   // data can be now index by column number or column name
+    var is_all_text = colnames.every(x => isNaN(Number(x)))   // check if there are only numbers
+    if (is_all_text) {
+        for (var k=0; k<colnames.length; ++k) {
+            data[k].shift()   // remove headline
+            data[colnames[k]] = data[k]   // data can be now indexed by column number or column name
         }
+    } else {
+        colnames = [...data.keys()].map((_, k) => k+1)   // one based indexing
     }
-    else {
-        data._colnames = [...data.keys()].map((_, i) => i+1)   // one based indexing
-    }
+    data._colnames = colnames
     data.unshift([...Array(data[0].length).keys()])  // prepend a column 0 with row numbers
     console.log(colnames)
 }
 
-function text_to_table(text, func, delim){
+function text_to_table(text, func, delim) {
    // guess the delimiter if not given
    lines = text.split("\n");
    if (delim) {}
@@ -46,4 +44,3 @@ function text_to_table(text, func, delim){
    data._basename = text._url.split('/').pop()
    func(data)
 }
-
